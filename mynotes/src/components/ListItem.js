1b6caac214321942a03851327483973e575ee918
TrajcheIvanov <79934583+TrajcheIvanov@users.noreply.github.com>
2021-10-18 22:34:03 +0200

    Added CSS

--- mynotes/src/components/ListItem.js
+++ mynotes/src/components/ListItem.js
@@ -4,9 +4,9 @@ import { Link  } from 'react-router-dom'
 const ListItem = ({note}) => {
     return (
         <Link to={`/note/${note.id}`}>
-        <div className="notes-list-item">
-            <h3>{note.body}</h3>
-        </div>
+            <div className="notes-list-item">
+                <h3>{note.body}</h3>
+            </div>
         </Link>
     )
 }
