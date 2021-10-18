7d0b5eddc8e47a9c3ad5ca9ffbfd4a4dd7086c3e
TrajcheIvanov <79934583+TrajcheIvanov@users.noreply.github.com>
2021-10-18 20:51:49 +0200

    initial commit

---
+++ mynotes/src/App.js
@@ -0,0 +1,19 @@
+import Header from './components/Header';
+import Body from './components/Body';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+        <Body />
+        <Footer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+ 
